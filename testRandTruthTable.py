efd978fa094788af60325abfb1b45813ebfc357d
Tim Ogden <togden1999@gmail.com>
2018-01-01 21:59:28 -0500

    Does not change the value of it's prediction, but its confidence fits perfectly with the correct data, so working almost perfectly :)

--- testRandTruthTable.py
+++ testRandTruthTable.py
@@ -2,12 +2,19 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense
 from keras.models import model_from_yaml
+
 text = input("Enter a 3 digits of 0 and 1's:")
 input_array = np.array([0,0,0])
 i = 0
 for digit in text:
 	input_array[i] = digit
 	i+=1;
+
+#Define number of classes
+num_classes = 2
+#Input dimensions
+rows, cols = 3, 1
+
 
 #load model yaml
 with open('model.yaml', 'r') as yaml_file:
@@ -20,5 +27,6 @@ loaded_model = model_from_yaml(loaded_yaml_model)
 loaded_model.load_weights('model_weights.h5')
 print("Loaded model and weights from disk")
 
-prediction = loaded_model.predict(input_array, batch_size=1, verbose=2)
+prediction = loaded_model.predict(np.array([[input_array[0],input_array[1],
+	input_array[2]]]), batch_size=1, verbose=2)
 print('Prediction: {} with {:.2f}% confidence.'.format(np.argmax(prediction), np.max(prediction)*100))
