679ab82f0bfb49d79d8c615d20ea09d755b57b55
Tim Ogden <togden1999@gmail.com>
2018-01-01 21:40:32 -0500

    Almost there to getting the test of the random truth table working

--- testRandTruthTable.py
+++ testRandTruthTable.py
@@ -1,7 +1,7 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense
-
+from keras.models import model_from_yaml
 text = input("Enter a 3 digits of 0 and 1's:")
 input_array = np.array([0,0,0])
 i = 0
@@ -9,5 +9,16 @@ for digit in text:
 	input_array[i] = digit
 	i+=1;
 
-for val in input_array:
-	print(val)
+#load model yaml
+with open('model.yaml', 'r') as yaml_file:
+	loaded_yaml_model = yaml_file.read()
+
+#Create model from yaml config
+loaded_model = model_from_yaml(loaded_yaml_model)
+
+#Load weights from trained network
+loaded_model.load_weights('model_weights.h5')
+print("Loaded model and weights from disk")
+
+prediction = loaded_model.predict(input_array, batch_size=1, verbose=2)
+print('Prediction: {} with {:.2f}% confidence.'.format(np.argmax(prediction), np.max(prediction)*100))
